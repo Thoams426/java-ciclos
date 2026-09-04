66906f7665d92129f190f00f992f1aee281483b7
MIGUEL ANGEL FRANCO MOLINA <u20261244439@usco.edu.co>
2026-09-04 11:39:34 -0500

    Add files via upload

---
+++ src/Ejercicios_Taller_2/MultiplosDeTres.java
@@ -0,0 +1,25 @@
+package Ejercicios_Taller_2;
+/*Ejercicio 3.a.10.Escribe un programa que muestre, cuente y sume los múltiplos de 3 que hay entre 1 y un número
+leído por teclado.*/
+import java.util.Scanner;
+public class MultiplosDeTres {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingresa un número entero positivo: ");
+        int num = sc.nextInt();
+        int cont = 0;
+        int suma = 0;
+        System.out.println("Los múltiplos de 3 entre 1 y " + num + " son:");
+        for (int i = 1; i <= num; i++) {
+           
+            if (i % 3 == 0) {
+                System.out.print(i + " ");
+                cont++; 
+                suma += i;    
+            }}
+        System.out.println();
+        System.out.println("Cantidad de múltiplos encontrados: " + cont);
+        
+        System.out.println("Suma total de los múltiplos: " + suma);
+
+        }}
